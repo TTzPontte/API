6610d148dabe599d10b9f5ea64e079df3c1cc6e4
Neto Chaves <netobac1@gmail.com>
2020-03-19 14:11:17 -0300

    feat: remove comments

--- src/layers/common/lambda/response.js
+++ src/layers/common/lambda/response.js
@@ -9,13 +9,6 @@ const headers = {
   'Content-Type': 'application/json'
 };
 
-/**
- * Build a correct response to API Gatway
- *
- * @param {Number} statusCode Status of request
- * @param {Object} body Data will pass in request
- */
-
 const build = statusCode => body => {
   return {
     headers,
@@ -24,12 +17,6 @@ const build = statusCode => body => {
   };
 };
 
-/**
- * Build a correct response error to API Gatway
- *
- * @param {Number} statusCode Status of request
- * @param {Object|String} msg Data will pass in request
- */
 const buildError = statusCode => msg => {
   return {
     headers,
@@ -41,67 +28,22 @@ const buildError = statusCode => msg => {
   };
 };
 
-/**
- * Build a response whit Status Code 201
- *
- * @param {Object|String} msg Data will pass in request
- */
 const created = build(201);
 
-/**
- * Build a response whit Status Code 200
- *
- * @param {Object|String} msg Data will pass in request
- */
 const success = build(200);
 
-/**
- * Build a response whit Status Code 204
- *
- * @param {Object|String} msg Data will pass in request
- */
 const noContent = build(204);
 
-/**
- * Build a response whit Status Code 400
- *
- * @param {Object|String} msg Data will pass in request
- */
 const badRequest = buildError(400);
 
-/**
- * Build a response whit Status Code 401
- *
- * @param {Object|String} msg Data will pass in request
- */
 const unauthorized = buildError(401);
 
-/**
- * Build a response whit Status Code 403
- *
- * @param {Object|String} msg Data will pass in request
- */
 const forbidden = buildError(403);
 
-/**
- * Build a response whit Status Code 404
- *
- * @param {Object|String} msg Data will pass in request
- */
 const notFound = buildError(404);
 
-/**
- * Build a response whit Status Code 406
- *
- * @param {Object|String} msg Data will pass in request
- */
 const notAcceptable = buildError(406);
 
-/**
- * Build a response whit Status Code 500
- *
- * @param {Object|String} msg Data will pass in request
- */
 const failure = buildError(500);
 
 module.exports = {
