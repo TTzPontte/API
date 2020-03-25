4de75008806074e9a9251bada6f668c466b7ff19
Neto Chaves <netobac1@gmail.com>
2020-03-25 11:20:31 -0300

    feat(authorization): refactor middleware

--- src/layers/common/middy/auth.js
+++ src/layers/common/middy/auth.js
@@ -2,23 +2,26 @@ const createError = require('http-errors');
 const jwt = require('jsonwebtoken');
 const Clients = require('../models/clients');
 
+const parseToken = token => {
+  if (!token) throw new createError.Unauthorized();
+  return token.replace('Bearer ', '');
+};
+
 module.exports = () => {
   return {
     before: async (handler, next) => {
       const { event } = handler;
-      let token = event.headers['Authorization'];
-      if (!token) throw new createError.Unauthorized();
-      token = token.slice(7, token.length); // Remove 'Bearer' from string
+      const token = parseToken(event.headers['Authorization']);
 
       try {
         const { clientId, body } = await jwt.decode(token);
         if (!clientId) throw new createError.Unauthorized();
         const { clientSecret, clientName } = await Clients.queryOne({ clientId }).exec();
+
         await jwt.verify(token, clientSecret);
         event.body = { ...body };
         event.clientId = clientId;
         event.clientName = clientName;
-
         next();
       } catch {
         throw new createError.Unauthorized();
