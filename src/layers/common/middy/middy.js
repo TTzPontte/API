8811a1f1b302448661176e87d401c801d643b899
Neto Chaves <netobac1@gmail.com>
2020-04-24 16:04:17 -0300

    feat: add a bodyparser middleware

--- src/layers/common/middy/middy.js
+++ src/layers/common/middy/middy.js
@@ -4,11 +4,13 @@ const { httpSecurityHeaders } = require('middy/middlewares');
 const httpErrorHandler = require('./httpErrorHandler');
 const xray = require('./xray');
 const auth = require('./auth');
+const parserBody = require('./parserBody');
 
 module.exports = handler => {
   return middy(handler)
     .use(xray())
     .use(httpSecurityHeaders())
     .use(httpErrorHandler())
-    .use(auth());
+    .use(auth())
+    .use(parserBody());
 };
---
+++ src/layers/common/middy/parserBody.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const createError = require('http-errors');
+
+module.exports = () => {
+  return {
+    before: async handler => {
+      const { event } = handler;
+      const { body, clientSecret } = event;
+      const { payload } = JSON.parse(body);
+      try {
+        const { body } = await jwt.verify(payload, clientSecret);
+        event.body = { ...body };
+      } catch (err) {
+        throw new createError.Unauthorized();
+      }
+      return;
+    }
+  };
+};
