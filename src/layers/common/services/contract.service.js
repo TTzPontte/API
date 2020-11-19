5a0bac6d04e574754e41f2a5c01b1c2481fef4f9
Yuri Mussi <ymussi@gmail.com>
2020-11-19 10:11:30 -0300

    refact(entity): debuging simulation contractOwners

--- src/layers/common/services/contract.service.js
+++ src/layers/common/services/contract.service.js
@@ -6,8 +6,8 @@ const Entity = require('./entity.service');
 const User = require('./user.service');
 const Process = require('./process.service');
 
-const getContractByOwner = async contractOwner => {
-  return ContractModel.query({ contractOwner: { eq: contractOwner } })
+const getContractByOwner = async contractOwners => {
+  return ContractModel.query({ contractOwners: { eq: contractOwners } })
     .using('ContractByOwner')
     .exec();
 };
@@ -18,7 +18,7 @@ const isRegistered = async ({ email, documentNumber }) => {
   if (entity && entity.length) {
     for (const person of entity) {
       console.log('person', person);
-      const contract = await getContractByOwner(person.id);
+      const contract = await getContractByOwner([person.id]);
       if (contract && contract.length) {
         throw new createError.Conflict('Customer already exists');
       }
@@ -39,18 +39,18 @@ const save = async ({ entity, property, lastContract, ...data }) => {
 
   const { User: cognitoUser } = await Cognito.createUser({ ...lastContract, ...simulation, loanValue, name, email, phone, documentNumber, id });
 
-  const { id: contractOwner } = await Entity.save(entity);
+  const { id: [contractOwners] } = await Entity.save(entity);
   const { id: propertyId } = await Property.save(property, trackCode);
 
   await User.save({
     id: cognitoUser.Username,
     trackingCode: trackCode,
-    peopleId: contractOwner,
+    peopleId: contractOwners[0],
     campaign: campaign,
     source: source
   });
 
-  const contract = new ContractModel({ ...lastContract, ...data, propertyId, contractOwner, source, campaign });
+  const contract = new ContractModel({ ...lastContract, ...data, propertyId, contractOwners, source, campaign });
   const savedContract = await contract.save();
 
   await Process.save({
