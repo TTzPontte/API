71cd0ed4ec6a23cb3116539e31f70e7fe7965ad2
Yuri Mussi <ymussi@gmail.com>
2020-12-03 16:21:49 -0300

    refact(API): debuging user.save again

--- src/layers/common/services/user.service.js
+++ src/layers/common/services/user.service.js
@@ -1,8 +1,9 @@
 const User = require('../models/user');
 
-const save = async ({ id, trackingCode, peopleId, campaign, source }) => {
+const save = async ({ id, cpf, trackingCode, peopleId, campaign, source }) => {
   const user = new User({
     id,
+    cpf,
     trackingCodes: [trackingCode],
     peopleId,
     campaign,
