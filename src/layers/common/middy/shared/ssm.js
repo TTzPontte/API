f2d08fc8ceb42eaa7a815166cdb345cc34f1b149
Yuri Mussi <ymussi@gmail.com>
2020-12-03 17:19:07 -0300

    refact(API): remove user pool id

--- src/layers/common/middy/shared/ssm.js
+++ src/layers/common/middy/shared/ssm.js
@@ -22,7 +22,6 @@ const ssmDefaultStatusGroup = () => {
     cacheExpiryInMillis: 3 * 60 * 60,
     names: {
         STATUS_GROUP_DEFAULT_ID: `/statusGroup/${ENV}/defaultId`,
-        COGNITO_USER_POOL_ID: `${prefix}user_pool_id`
     }
   });
 };
