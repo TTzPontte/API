24ada174a590c04e8babc5d163f51605937101e7
Yuri Mussi <ymussi@gmail.com>
2020-12-03 14:37:27 -0300

    refact(API): debuging user.save

--- src/layers/common/services/contract.service.js
+++ src/layers/common/services/contract.service.js
@@ -40,6 +40,15 @@ const save = async ({ entity, property, lastContract, ...data }) => {
   const { id: contractOwner } = await Entity.save(entity);
   const { id: propertyId } = await Property.save(property, trackCode);
 
+  // Log to debug User.save
+  console.log('User.save -> ', {
+    id: cognitoUser.Username,
+    trackingCode: trackCode,
+    peopleId: contractOwner,
+    campaign: campaign,
+    source: source
+  });
+
   await User.save({
     id: cognitoUser.Username,
     trackingCode: trackCode,
