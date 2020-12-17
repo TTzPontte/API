4d761d1d5f071d1250349830cfd1b451e79cae64
Yuri Mussi <ymussi@gmail.com>
2020-12-17 16:40:48 -0300

    refact: ssm and add statusGroupContractId

--- src/layers/common/middy/shared/ssm.js
+++ src/layers/common/middy/shared/ssm.js
@@ -15,14 +15,12 @@ const ssmCognito = () => {
 };
 
 const ssmDefaultStatusGroup = () => {
-  const prefix = `/cognito/${ENV}/`;
 
   return ssm({
     cache: true,
     cacheExpiryInMillis: 3 * 60 * 60,
     names: {
-        STATUS_GROUP_DEFAULT_ID: `/statusGroup/${ENV}/defaultId`,
-        COGNITO_USER_POOL_ID: `${prefix}user_pool_id`
+        STATUS_GROUP_DEFAULT_ID: `/statusGroup/${ENV}/defaultId`
     }
   });
 };
