c7f3348fda73dd416d3fe774ca848e2ff7a287a2
Yuri Mussi <ymussi@gmail.com>
2020-12-17 23:06:22 -0300

    refact(ssm): debug ssmDefaultStatusGroup

--- src/contracts/contract.js
+++ src/contracts/contract.js
@@ -8,20 +8,20 @@ const translateBody = require('./translate');
 const { ssmCognito, ssmDefaultStatusGroup } = require(`${path}/middy/shared/ssm`);
 
 const contract = async event => {
-  console.log('ssmDefaultStatusGroup -> ', ssmDefaultStatusGroup);
-
+  
   const { body, clientId } = event;
   const { simulationId } = body;
-
+  
   await validate({ ...body, clientId });
-
+  
   const translatedBody = translateBody(body);
-
+  
   const lastContract = await Simulation.getLastContract(simulationId);
-
+  
   const contract = await Contract.save({ ...translatedBody, clientId, lastContract });
-
+  
   return success({ ...contract });
 };
+console.log('ssmDefaultStatusGroup -> ', { handler: middy(contract).use(ssmCognito()), contract });
 
 module.exports = { handler: middy(contract).use(ssmCognito()), contract };
