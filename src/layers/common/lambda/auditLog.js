b5207e080cc5c367738fb1353a4b1361540de082
Yuri Mussi <ymussi@gmail.com>
2021-01-16 01:02:07 -0300

    feat(ecred): refact auditlog

--- src/layers/common/lambda/auditLog.js
+++ src/layers/common/lambda/auditLog.js
@@ -1,21 +1,38 @@
 const uuid = require('uuid/v1');
-const { sendMessage } = require('../aws/sqs');
+const { SQS } = require('../aws');
 
 const { ENV } = process.env;
 
-const log = async ({ requestId, event, context, operation, data }) => {
-  const nameSQS = `ApiAuditLog_${ENV}`;
+const log = async ({ requestContext = {} }, context, document, operation, data = {}) => {
+  const nameSQS = `AuditLog_${ENV}`;
   const accountId = context.invokedFunctionArn.split(':')[4];
+  const user = (requestContext && requestContext.authorizer && requestContext.authorizer.claims) || {};
+  const sourceIp = (requestContext && requestContext.identity && requestContext.identity.sourceIp) || {};
 
   const body = {
+    uid: user['custom:uid'] || 'system',
     id: uuid(),
-    ts: new Date().toISOString(),
-    ip: event.requestContext.identity.sourceIp,
+    document,
     operation,
-    data
+    ts: new Date().toISOString(),
+    ip: sourceIp || '0.0.0.0',
+    email: user.email || '',
+    user: {
+      name: user.name || '',
+      picture: user.picture || ''
+    }
   };
 
-  const result = await sendMessage(JSON.stringify(body), accountId, nameSQS);
+  if (data.cid) {
+    body.cid = data.cid;
+    delete data.cid;
+  }
+
+  if (data) {
+    body.data = data;
+  }
+
+  const result = await SQS.sendMessage(JSON.stringify(body), accountId, nameSQS);
   return result;
 };
 
