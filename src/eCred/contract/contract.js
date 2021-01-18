415b3904ef7b7e66b2eb259d6097e4dc708af2f4
Yuri Mussi <ymussi@gmail.com>
2021-01-18 19:14:38 -0300

    feat(ecred): teste endpoint

--- src/eCred/contract/contract.js
+++ src/eCred/contract/contract.js
@@ -3,7 +3,9 @@ const middy = require(`${path}/middy/middy`);
 const { ssmCognito } = require(`${path}/middy/shared/ssm`);
 
 const contract = async event => {
-  return 'Hello World';
+  const { clientId } = event;
+
+  return clientId;
 };
 
 module.exports = { handler: middy(contract).use(ssmCognito()), contract };
