db799f8fca6afa98c531b821444e7b12f3a27b87
Yuri Mussi <ymussi@gmail.com>
2021-01-19 16:36:49 -0300

    feat(ecred): add exceptions calculator

--- src/layers/common/helpers/exceptions.js
+++ src/layers/common/helpers/exceptions.js
@@ -3,7 +3,7 @@ const createError = require('http-errors');
 const exceptionsCalculator = async response => {
   console.log('response -> ', response);
   if (response.statusCode) {
-    throw new createError.badRequest(
+    throw new createError.BadRequest(
       "Sorry, we had a problem with one of our services but don't worry because we are already working to solve this problem as fast as possible."
     );
   }
