3462fe771a784d53d7fc6a76233e0922a9b65bdd
Yuri Mussi <ymussi@gmail.com>
2021-01-19 16:36:42 -0300

    feat(ecred): add exceptions calculator

--- src/layers/common/helpers/exceptions.js
+++ src/layers/common/helpers/exceptions.js
@@ -1,9 +1,9 @@
-const path = process.env.NODE_ENV === 'test' ? '../../layers/common' : '/opt';
-const { badRequest } = require(`${path}/lambda/response`);
+const createError = require('http-errors');
 
 const exceptionsCalculator = async response => {
+  console.log('response -> ', response);
   if (response.statusCode) {
-    return badRequest(
+    throw new createError.badRequest(
       "Sorry, we had a problem with one of our services but don't worry because we are already working to solve this problem as fast as possible."
     );
   }
