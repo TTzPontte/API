403fca067d7b5f3aaff0b0afa466d89198bc526f
Yuri Mussi <ymussi@gmail.com>
2021-01-20 15:10:22 -0300

    fix(ecred): remove duplicated validation onelasticsearch

--- src/layers/common/services/offer.service.js
+++ src/layers/common/services/offer.service.js
@@ -1,6 +1,4 @@
-const createError = require('http-errors');
 const ContractModel = require('../models/contract');
-const { getEntity } = require('../elasticsearch/entity.es');
 const Entity = require('./entity.service');
 const Process = require('./process.service');
 
@@ -14,23 +12,7 @@ const setEntityType = documentNumber => {
   return documentNumber.length === 14 ? 'PJ' : 'PF';
 };
 
-const isRegistered = async ({ email, documentNumber }) => {
-  const entity = await getEntity({ email, documentNumber });
-
-  if (entity && entity.length) {
-    for (const person of entity) {
-      const contract = await getContractByOwner(person.id);
-      if (contract && contract.length) {
-        throw new createError.Conflict('Customer already exists');
-      }
-    }
-  }
-  return false;
-};
-
 const save = async ({ entity, lastContract, ...data }) => {
-  await isRegistered(entity);
-
   const { documentNumber } = entity;
   const { source, campaign } = lastContract;
   const entityType = setEntityType(documentNumber);
@@ -55,4 +37,4 @@ const save = async ({ entity, lastContract, ...data }) => {
   return savedContract;
 };
 
-module.exports = { save, isRegistered, getContractByOwner };
+module.exports = { save, getContractByOwner };
