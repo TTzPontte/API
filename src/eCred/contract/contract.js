3854b874bdc8db0ddb3cebb193549ae4058de1d9
Yuri Mussi <ymussi@gmail.com>
2021-01-20 16:57:14 -0300

    feat(ecred): add validations on /contract

--- src/eCred/contract/contract.js
+++ src/eCred/contract/contract.js
@@ -1,11 +1,16 @@
 const path = process.env.NODE_ENV === 'test' ? '../../layers/common' : '/opt';
+const { validate } = require('./validator');
 const middy = require(`${path}/middy/middy`);
+const { success } = require(`${path}/lambda/response`);
 const { ssmCognito } = require(`${path}/middy/shared/ssm`);
 
 const contract = async event => {
-  const { clientId } = event;
+  const { body, clientId } = event;
+  const { proposal_id } = body.order;
 
-  return clientId;
+  await validate({ ...body, clientId });
+
+  return success({ proposal_id });
 };
 
 module.exports = { handler: middy(contract).use(ssmCognito()), contract };
