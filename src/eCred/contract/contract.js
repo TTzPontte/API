dd1801391b5f84316c6530c1a17f65acc30ce163
Yuri Mussi <ymussi@gmail.com>
2021-01-21 16:27:29 -0300

    feat(ecred): update api eCred

--- src/eCred/contract/contract.js
+++ src/eCred/contract/contract.js
@@ -1,11 +1,37 @@
 const path = process.env.NODE_ENV === 'test' ? '../../layers/common' : '/opt';
+const { validate } = require('./validator');
+const Simulation = require(`${path}/services/simulation.service`);
+const Offer = require(`${path}/services/offer.service`);
+const { parserBody, parserLastContract, parserResponseContract } = require('./parser');
 const middy = require(`${path}/middy/middy`);
+const translateBody = require('./translate');
+const { success } = require(`${path}/lambda/response`);
 const { ssmCognito } = require(`${path}/middy/shared/ssm`);
 
 const contract = async event => {
-  const { clientId } = event;
+  const { body, clientId } = event;
 
-  return clientId;
+  const { proposal_id } = body.order;
+
+  await validate({ ...body, clientId });
+
+  const translatedBody = translateBody(body);
+
+  const bodyParsed = parserBody(translatedBody);
+
+  const { documentNumber } = bodyParsed.entity;
+
+  const lastContract = await Simulation.getLastContract(proposal_id);
+
+  const lastContractParsed = parserLastContract({ lastContract, bodyParsed });
+
+  const lastEntity = await Offer.getLastEntity({ documentNumber });
+
+  const contract = await Offer.saveContract({ ...bodyParsed, clientId, lastContract: lastContractParsed, lastEntity });
+
+  const response = parserResponseContract({ ...contract });
+
+  return success(response);
 };
 
 module.exports = { handler: middy(contract).use(ssmCognito()), contract };
--- src/layers/common/elasticsearch/contractsReport.es.js
+++ src/layers/common/elasticsearch/contractsReport.es.js
@@ -39,4 +39,38 @@ const getClientContract = async ({ documentNumber, email, clientId }) => {
   return data;
 };
 
-module.exports = { getClientContract };
+const getClientContractByDocNumber = async ({ documentNumber, email, clientId }) => {
+  const query = {
+    index: 'contract',
+    type: 'report',
+    body: {
+      query: {
+        bool: {
+          should: [
+            {
+              term: {
+                'simulation.parameters.cpf': documentNumber
+              }
+            }
+          ],
+          must_not: {
+            match: {
+              clientApiId: clientId
+            }
+          }
+        }
+      }
+    }
+  };
+  const { body } = await es.search(query);
+
+  const data = [];
+  for (const hits of body.hits.hits) {
+    const source = hits._source;
+    data.push({ ...source });
+  }
+
+  return data;
+};
+
+module.exports = { getClientContract, getClientContractByDocNumber };
