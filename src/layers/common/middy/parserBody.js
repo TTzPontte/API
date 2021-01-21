3d762c9f9a95c01e6f79cd3ba7ee6b703c065f13
Yuri Mussi <ymussi@gmail.com>
2021-01-21 01:54:28 -0300

    feat(ecred): refact update entity

--- src/layers/common/middy/parserBody.js
+++ src/layers/common/middy/parserBody.js
@@ -8,10 +8,6 @@ module.exports = () => {
       const { body, clientSecret } = event;
       const { payload } = JSON.parse(body);
 
-      console.log('body', body);
-      console.log('payload', payload);
-      console.log('clientSecret', clientSecret);
-
       if (!payload) return;
 
       try {
