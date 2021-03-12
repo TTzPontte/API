80186975794f53b1a2c10167ef2ad311e670aa81
Hugo <hugo@pontte.com.br>
2021-03-12 17:15:47 -0300

    chore: use learna for monorepo

--- install-layer-deps.sh
+++ install-layer-deps.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 rm -rf layer
 mkdir -p layer/nodejs
-cp ./package* ./layer/nodejs/
+cp ./packag*.json ./layer/nodejs/
 cd ./layer/nodejs/
 pwd
+sed -i '/"postinstall"/d' ./package.json
 npm ci --only=prod --unsafe-perm
 cd - 
 pwd
