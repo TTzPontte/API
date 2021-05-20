9fd91f251cf117051411a711a34d600a6f11bcd1
Hugo <hugo@pontte.com.br>
2021-05-20 17:35:41 -0300

    fix: change sac to upper case

--- packages/eCred/offer/parser.js
+++ packages/eCred/offer/parser.js
@@ -83,7 +83,7 @@ const parserResponseOfferSimulation = ({ simulationId, calculated }) => {
       installments_value: calculated.installment[0].installment,
       total_payable: calculated.grossLoan,
       fee_credit_opening: calculated.registry_value,
-      depreciation_system: 'Sac',
+      depreciation_system: 'SAC',
       first_installment_value: calculated.installment[0].installment,
       fee_administration: 0.0
     }
