38d07a16715a75814342116f322d13ba36900a93
Hugo <hugo@pontte.com.br>
2021-06-04 16:42:15 -0300

    fix: save contract as registred

--- packages/common/models/uid.js
+++ packages/common/models/uid.js
@@ -3,9 +3,14 @@ const NAMESPACE_OID = '6ba7b812-9dad-11d1-80b4-00c04fd430c8';
 
 const remove_non_alphanum = value => value.toString().replace(/[^0-9a-zA-Z]+/g);
 
-exports.idOf = value => {
-  const uid = uuid3(NAMESPACE_OID, remove_non_alphanum(value), []);
-  return Buffer.from(uid)
+const urlEncode = data =>
+  Buffer.from(data)
     .toString('base64')
-    .replace(/=+/g, '');
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+
+exports.idOf = value => {
+  const uid = uuid3(remove_non_alphanum(value), NAMESPACE_OID, []);
+  return urlEncode(uid);
 };
